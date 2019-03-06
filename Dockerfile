194237d180221709be96703106a1730ee09a6334
Your Name <aaa>
2019-03-06 23:50:34 +0800

    update bb

--- Dockerfile
+++ Dockerfile
@@ -50,6 +50,7 @@ RUN set -ex \
         procps \
         vim \
         net-tools \
+        sudo \
     && sed -i 's/^# en_US.UTF-8 UTF-8$/en_US.UTF-8 UTF-8/g' /etc/locale.gen \
     && locale-gen \
     && update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 \
@@ -60,7 +61,8 @@ RUN set -ex \
     && pip install ndg-httpsclient \
     && pip install pyasn1 \
     && pip install apache-airflow[crypto,celery,postgres,hive,jdbc,mysql,ssh${AIRFLOW_DEPS:+,}${AIRFLOW_DEPS}]==${AIRFLOW_VERSION} \
-    && pip install 'redis>=2.10.5,<3' \
+    && pip install 'redis>=3.2.0' \
+    && pip install 'tornado<6.0' \
     && if [ -n "${PYTHON_DEPS}" ]; then pip install ${PYTHON_DEPS}; fi \
     && apt-get purge --auto-remove -yqq $buildDeps \
     && apt-get autoremove -yqq --purge \
@@ -76,10 +78,11 @@ RUN set -ex \
 COPY script/entrypoint.sh /entrypoint.sh
 COPY config/airflow.cfg ${AIRFLOW_HOME}/airflow.cfg
 
-RUN chown -R airflow: ${AIRFLOW_HOME}
-RUN mkdir -p ${AIRFLOW_HOME}/logs ${AIRFLOW_HOME}/logs/dag_processor_manager
+#RUN mkdir -p ${AIRFLOW_HOME}/logs ${AIRFLOW_HOME}/logs/dag_processor_manager
+VOLUME ${AIRFLOW_HOME}/logs
+RUN chown -R airflow: ${AIRFLOW_HOME} ${AIRFLOW_HOME}/logs
 
-EXPOSE 8181 5555 8793
+EXPOSE 8080 5555 8793
 
 USER airflow
 WORKDIR ${AIRFLOW_HOME}
--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,15 +2,9 @@
 
 TRY_LOOP="20"
 
-: "${REDIS_HOST:="redis"}"
-: "${REDIS_PORT:="6379"}"
+: "${REDIS_HOST:="rm22928.hebe.grid.sina.com.cn"}"
+: "${REDIS_PORT:="22928"}"
 : "${REDIS_PASSWORD:=""}"
-
-: "${POSTGRES_HOST:="postgres"}"
-: "${POSTGRES_PORT:="5432"}"
-: "${POSTGRES_USER:="airflow"}"
-: "${POSTGRES_PASSWORD:="airflow"}"
-: "${POSTGRES_DB:="airflow"}"
 
 # Defaults and back-compat
 : "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
@@ -23,7 +17,6 @@ export \
   AIRFLOW__CORE__FERNET_KEY \
   AIRFLOW__CORE__LOAD_EXAMPLES \
   AIRFLOW__CORE__SQL_ALCHEMY_CONN \
-
 
 # Load DAGs exemples (default: Yes)
 if [[ -z "$AIRFLOW__CORE__LOAD_EXAMPLES" && "${LOAD_EX:=n}" == n ]]
@@ -42,29 +35,13 @@ else
     REDIS_PREFIX=
 fi
 
-wait_for_port() {
-  local name="$1" host="$2" port="$3"
-  local j=0
-  while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
-    j=$((j+1))
-    if [ $j -ge $TRY_LOOP ]; then
-      echo >&2 "$(date) - $host:$port still not reachable, giving up"
-      exit 1
-    fi
-    echo "$(date) - waiting for $name... $j/$TRY_LOOP"
-    sleep 5
-  done
-}
-
 if [ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" ]; then
-  AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
-  AIRFLOW__CELERY__RESULT_BACKEND="db+postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
-  wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
+  AIRFLOW__CORE__SQL_ALCHEMY_CONN="mysql://airflow:airflow@m3304i.mars.grid.sina.com.cn:3304/airflow" 
+  AIRFLOW__CELERY__RESULT_BACKEND="redis://$REDIS_PREFIX$REDIS_HOST:$REDIS_PORT/0"
 fi
 
 if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
-  AIRFLOW__CELERY__BROKER_URL="redis://$REDIS_PREFIX$REDIS_HOST:$REDIS_PORT/1"
-  wait_for_port "Redis" "$REDIS_HOST" "$REDIS_PORT"
+  AIRFLOW__CELERY__BROKER_URL="redis://$REDIS_PREFIX$REDIS_HOST:$REDIS_PORT/0"
 fi
 
 case "$1" in
@@ -72,18 +49,21 @@ case "$1" in
     airflow initdb
     if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ]; then
       # With the "Local" executor it should all run in one container.
-      airflow scheduler --debug &
+      airflow scheduler --debug -l /usr/local/airflow/logs/webserver.log &
     fi
-    exec airflow webserver --debug
+    exec airflow webserver --debug --log-file /usr/local/airflow/logs/webserver.log -A /usr/local/airflow/logs/webserver.access -E /usr/local/airflow/logs/webserver.err
     ;;
-  worker|scheduler)
-    # To give the webserver time to run initdb.
+  worker)
     sleep 10
-    exec airflow "$@"
+    exec airflow worker --log-file /usr/local/airflow/logs/worker.log
+    ;;
+  scheduler)
+    sleep 10
+    exec airflow scheduler --log-file /usr/local/airflow/logs/scheduler.log
     ;;
   flower)
     sleep 10
-    exec airflow "$@"
+    exec airflow flower --log-file /usr/local/airflow/logs/flower.log
     ;;
   version)
     exec airflow "$@"
@@ -91,6 +71,6 @@ case "$1" in
   *)
     # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
     # exec "$@"
-    exec sleep 9999999999999
+    exec sleep 999999999999999
     ;;
 esac
